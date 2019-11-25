e185a83d1b547b9fccbec947b10d590769b372ef
kisslaci98 <klaci9898@gmail.com>
2019-11-25 12:30:14 +0100

    1. modositas

---
+++ Tombok/Tombok/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tombok
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //42.feladat - 43.feladat
+            /*Console.Write("Mennyi adattal dolgozzak: ");
+            int darabszam = Int32.Parse(Console.ReadLine());
+
+            int[] tomb = new int[darabszam];
+
+            for (int i = 0; i < tomb.Length; i++)
+            {
+                Console.Write($"Kérem a(z) {i + 1}. számot: ");
+                int szam = Int32.Parse(Console.ReadLine());
+                tomb[i] = szam;
+            }
+            //Megszámoljuk a páratlan tömbelemeket.
+            int paratlanokSzama = 0;
+
+            for (int i = 0; i < tomb.Length; i++)
+            {
+                if (tomb[i] % 2 == 1)
+                {
+                    paratlanokSzama++;
+                }
+            }
+            Console.WriteLine($"A tömbben található páratlan számok száma: {paratlanokSzama}");
+
+            //Páros számok összege
+            int parosokOsszege = 0;
+
+            for (int i = 0; i < tomb.Length; i++)
+            {
+                if (tomb[i] % 2 == 0)
+                {
+                    parosokOsszege += tomb[i];
+                }
+            }
+            Console.WriteLine($"A tömbben található páros számok összege: {parosokOsszege}");
+
+            Console.ReadKey(true);*/
+
+            //44.feladat
+            
+        }
+    }
+}
