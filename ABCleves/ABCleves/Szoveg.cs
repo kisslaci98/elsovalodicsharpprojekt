79267a9d5044b244eb4eb5af347c10d924430984
kisslaci98 <klaci9898@gmail.com>
2020-01-28 14:45:17 +0100

    commit numero 1

---
+++ ABCleves/ABCleves/Szoveg.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ABCleves
+{
+    class Szoveg
+    {
+        public string Bemeno { get; set; }
+
+        public Szoveg(string bemeno)
+        {
+            this.Bemeno = bemeno;
+        }
+
+        static string Sorrendbe_2(string bemeno)
+        {
+            string kimeno = "ß";
+
+            for (int i = 0; i < bemeno.Length; i++)
+            {
+                Betuk be;
+                Enum.TryParse(bemeno[i].ToString(), true, out be);
+                for (int j = 0; j < kimeno.Length; j++)
+                {
+                    Betuk ki;
+                    Enum.TryParse(kimeno[j].ToString(), true, out ki);
+                    if (bemeno[i] < kimeno[j])
+                    {
+                        kimeno = String.Concat(kimeno.Substring(0, j), bemeno[i], kimeno.Substring(j));
+                        break;
+                    }
+
+                    kimeno = String.Concat(kimeno, bemeno[i]);
+                }
+            }
+
+            return kimeno.Substring(0, bemeno.Length);
+        }
+    }
+}
