0a6d9be1228e34ac0a55cd8d6bd8e5e472b1c68e
kisslaci98 <klaci9898@gmail.com>
2020-01-28 14:05:30 +0100

    Doga megoldás

---
+++ Dolgozat_3/Dolgozat_3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dolgozat_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str, str1 = "";
+            int i, l;
+
+            Console.Write("Kérek egy mondatot: ");
+            str = Console.ReadLine();
+
+            l = str.Length - 1;
+            for (i = l; i >= 0; i--)
+            {
+
+                str1 = str1 + str[i];
+
+
+            }
+
+            Console.WriteLine(str1);
+            
+
+            Console.ReadKey(true);
+        }
+    }   
+}
---
+++ ReverseString/ReverseString/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Kérem a sztringet: ");
+            string bemeno = Console.ReadLine();
+            Szoveg szov = new Szoveg(bemeno);
+
+            Console.WriteLine($"A megfordított sztring: {Megfordit(bemeno)}");
+            Console.WriteLine($"A megfordított sztring: {szov.Megfordit()}");
+
+            Console.ReadKey(true);
+        }
+        static string Megfordit(string bemeno)
+        {
+            string kimeno = "";
+
+            for (int i = bemeno.Length - 1; i >= 0; i--)
+            {
+                kimeno = String.Concat(kimeno, bemeno[i]);
+            }
+
+            return kimeno;
+        }
+    }
+
+    class Szoveg
+    {
+        public string Bemeno { get; set; }
+
+        public Szoveg(string bemeno)
+        {
+            this.Bemeno = bemeno;
+        }
+
+        public string Megfordit()
+        {
+            string kimeno = "";
+
+            for (int i = this.Bemeno.Length - 1; i >= 0; i--)
+            {
+                kimeno = String.Concat(kimeno, this.Bemeno[i]);
+            }
+
+            return kimeno;
+        }
+    }
+}
